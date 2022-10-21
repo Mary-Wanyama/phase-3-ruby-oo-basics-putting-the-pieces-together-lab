bc7f14f971361d7d68e4da4e216b3877e816c5cf
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-21 08:03:07 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_accessor :title, :genre, :page_count, :author
+    
+def initialize(title)
+    @title = title
+end
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
 
-
+   
+end
+book = Book.new("And Then There Were None")
